657225970f64a526793f3874c8ee31a85cc4c6a5
Rease <rease02@gmail.com>
2022-09-22 15:45:03 -0400

    Character

---
+++ exercises/character_creation/__init__.py
@@ -0,0 +1,33 @@
+from turtle import color
+
+
+class Citizen_Character():
+    def __init__(self) -> None:
+        pass
+        self.color = ''
+        self.clothes = ''
+        self.annoying = True
+        self.money_amount = 0
+        self.IQ = 2
+        self.can_die = True
+        self.inventory_list = []
+
+
+class Merchant_Character():
+    def __init__(self) -> None:
+        pass
+        self.mysterious = True
+        self.profession = ''
+        self.wares = []
+        self.has_weaponry = True
+
+
+class Guard_Character():
+    pass
+
+class Soldier_Character():
+    pass
+
+class Noble_Character():
+    pass
+
