3948af51ba550746738ce7fda91591f30dc213aa
Rease <rease02@gmail.com>
2023-02-08 15:44:01 -0500

    im going to make a nuke in my backyard using radioactive materials i steal from smoke detectors in hotels

--- daily_code_problems/nuclear_mitigation_execise/conditionals.py
+++ daily_code_problems/nuclear_mitigation_execise/conditionals.py
@@ -1,8 +1,9 @@
 """Functions to prevent a nuclear meltdown."""
 
 
-def is_criticality_balanced(temperature, neutrons_emitted):
+def is_criticality_balanced(temperature: int|float, neutrons_emitted: int|float) -> bool:
     """Verify criticality is balanced.
+
 
     :param temperature: int or float - temperature value in kelvin.
     :param neutrons_emitted: int or float - number of neutrons emitted per second.
@@ -13,8 +14,12 @@ def is_criticality_balanced(temperature, neutrons_emitted):
     - The number of neutrons emitted per second is greater than 500.
     - The product of temperature and neutrons emitted per second is less than 500000.
     """
+    if temperature < 800 and neutrons_emitted > 500 and temperature * neutrons_emitted < 500000:
+        return True
+    else:
+        return False
 
-    pass
+    
 
 
 def reactor_efficiency(voltage, current, theoretical_max_power):
@@ -36,8 +41,18 @@ def reactor_efficiency(voltage, current, theoretical_max_power):
     (generated power/ theoretical max power)*100
     where generated power = voltage * current
     """
+    generated_power = voltage * current
+    efficiency = (generated_power/theoretical_max_power) * 100
+    if efficiency >= 80:
+        return 'green'
+    elif efficiency < 80 and efficiency >= 60:
+        return 'orange'
+    elif efficiency < 60 and efficiency >= 30:
+        return 'red'
+    elif efficiency < 30:
+        return 'black'
 
-    pass
+    
 
 
 def fail_safe(temperature, neutrons_produced_per_second, threshold):
@@ -53,4 +68,10 @@ def fail_safe(temperature, neutrons_produced_per_second, threshold):
     3. 'DANGER' -> `temperature * neutrons per second` is not in the above-stated ranges
     """
 
-    pass
+    status_result = temperature * neutrons_produced_per_second
+    if status_result < threshold * .9:
+        return 'LOW'
+    elif status_result < threshold + threshold * .10 and status_result > threshold - threshold * .10:
+        return 'NORMAL'
+    else:
+        return 'DANGER'
